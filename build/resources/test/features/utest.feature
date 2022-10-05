34148ff02995399efd84de50822e50cf52fe33b1
cCastroVa <ccastrova@choucairtesting.com>
2022-10-05 15:40:30 -0500

    version 1.3

--- build/resources/test/features/utest.feature
+++ build/resources/test/features/utest.feature
@@ -6,8 +6,17 @@ Feature: Join the community of testers.
   Scenario: Register on the Utest.com page
     Given than Carlos is on the Utest page
     When he clicks Join Today on the Utest page
-    And he enters the data required in the four registration steps
-      | first_name | last_name | email_address     | month_bird | day_bird | year_bird | language | city             | postal_code | country  | computer | version              | language_computer   | mobile_device | model        | operating_system | password       | repeat_password   |
-      | Carlos     | Castro    | castro@correo.com | December   | 3        | 1997      | Spanish  | Bello, Antioquia | 050044      | Colombia | Windows  | Windows 10 Education | Spanish             | Motorola      | Moto G9 Plus | Android 10       | Asdfghjkl123*a | Asdfghjkl123*a    |
+    And he enters the data required in the first registration step
+      | first_name | last_name | email_address     | month_bird | day_bird | year_bird | language |
+      | Carlos     | Castro    | castro@correochouc.com | December   | 3        | 1997      | Spanish  |
+    And he enters the data required in the second registration step
+      | city            | postal_code | country |
+      |Bello, Antioquia | 050044      | Colombia|
+    And he enters the data required in the third registration step
+      | computer | version              | language_computer   | mobile_device | model        | operating_system |
+      | Windows  | Windows 10 Education | Spanish             | Motorola      | Moto G9 Plus | Android 10       |
+    And he enters the data required in the fourth registration step
+      |password        | repeat_password   |
+      | Asdfghjkl123*a | Asdfghjkl123*a    |
     And he accept the terms of use, the guidelines and the privacy and security policy of utest
     Then he registers successful on the Utest page seeing a message First, please check your email inbox
--- src/test/java/stepdefinitions/UtestStepDefinition.java
+++ src/test/java/stepdefinitions/UtestStepDefinition.java
@@ -27,17 +27,28 @@ public class UtestStepDefinition {
     public void heClicksJoinTodayOnTheUtestPage() {
         OnStage.theActorInTheSpotlight().attemptsTo(JoinToday.the());
     }
-    @When("^he enters the data required in the four registration steps$")
-    public void heEntersTheDataRequiredInTheFourRegistrationSteps(List<DataRequired>requiredList) {
-        OnStage.theActorInTheSpotlight().attemptsTo(Enters.theData(requiredList.get(0).getFirst_name(),
-                        requiredList.get(0).getLast_name(), requiredList.get(0).getEmail_address(), requiredList.get(0)
+    @When("^he enters the data required in the first registration step$")
+    public void heEntersTheDataRequiredInTheFirstRegistrationStep(List<DataRequired>requiredList) {
+        OnStage.theActorInTheSpotlight().attemptsTo(Enters.theData(requiredList.get(0).getFirst_name(), requiredList
+                        .get(0).getLast_name(), requiredList.get(0).getEmail_address(), requiredList.get(0)
                         .getMonth_bird(), requiredList.get(0).getDay_bird(), requiredList.get(0).getYear_bird(),
-                requiredList.get(0).getLanguage()), (EntersTwo.theData(requiredList.get(0).getCity(), requiredList.get(0)
-                        .getPostal_code(), requiredList.get(0).getCountry())),
-                (EntersThree.theData(requiredList.get(0).getComputer(), requiredList.get(0).getVersion(),requiredList
-                        .get(0).getLanguage_computer(), requiredList.get(0).getMobile_device(), requiredList.get(0)
-                        .getModel(), requiredList.get(0).getOperating_system())), EntersFour.theData(requiredList.get(0)
-                        .getPassword(), requiredList.get(0).getRepeat_password()));
+                requiredList.get(0).getLanguage()));
+    }
+    @When("^he enters the data required in the second registration step$")
+    public void heEntersTheDataRequiredInTheSecondRegistrationStep(List<DataRequired>requiredList) {
+        OnStage.theActorInTheSpotlight().attemptsTo(EntersTwo.theData(requiredList.get(0).getCity(), requiredList.get(0)
+                        .getPostal_code(), requiredList.get(0).getCountry()));
+    }
+    @When("^he enters the data required in the third registration step$")
+    public void heEntersTheDataRequiredInTheThirdRegistrationStep(List<DataRequired>requiredList) {
+        OnStage.theActorInTheSpotlight().attemptsTo(EntersThree.theData(requiredList.get(0).getComputer(), requiredList
+                .get(0).getVersion(), requiredList.get(0).getLanguage_computer(), requiredList.get(0).getMobile_device(),
+                requiredList.get(0).getModel(), requiredList.get(0).getOperating_system()));
+    }
+    @When("^he enters the data required in the fourth registration step$")
+    public void heEntersTheDataRequiredInTheFourthRegistrationStep(List<DataRequired>requiredList) {
+        OnStage.theActorInTheSpotlight().attemptsTo(EntersFour.theData(requiredList.get(0)
+                .getPassword(), requiredList.get(0).getRepeat_password()));
     }
     @When("^he accept the terms of use, the guidelines and the privacy and security policy of utest$")
     public void heAcceptTheTermsOfUseTheGuidelinesAndThePrivacyAndSecurityPolicyOfUtest() {
@@ -47,5 +58,4 @@ public class UtestStepDefinition {
     public void heRegistersSuccessfulOnTheUtestPageSeeingAMessageCheckYourEmailInbox(String question) {
         OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(Answer.toThe(question)));
     }
-
 }
